e49bb363577d709610d94a16a83e997c112207b0
Sascha-Bausch <85745172+Sascha-Bausch@users.noreply.github.com>
2021-12-24 16:06:19 +0100

    Add files via upload

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="about">
     <h1>A site 4 kick-ass events!!!</h1>
+    <h2>Another Thing:</h2>
   </div>
+  <div id="app">
+    <h2>{{ date }}</h2>
+    <p>You clicked the button {{ numberOfClicks }} times</p>
+    <button @click="clickButton">Click me!</button>
+    <p v-if='numberOfClicks > 0'>The button was last clicked at {{ lastClick }}</p>
+    </div>
 </template>
+
+<script>
+import {ref, onMounted, watch} from 'vue'
+export default {
+  setup(){
+    const numberOfClicks = ref(0)
+    function clickButton() {
+      numberOfClicks.value++
+    }
+  
+    const lastClick = ref('')
+    watch(numberOfClicks, () => {
+      lastClick.value = new Date().toTimeString()
+    })
+
+    let date = ref('')
+    onMounted(() => {
+      date.value = new Date().toDateString()
+    })
+
+    return {
+      date,
+      numberOfClicks,
+      clickButton,
+      lastClick
+    }
+  }
+};
+</script>
